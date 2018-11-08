1940441a33dec64b9b5115d5df29c60524c1ef2e
Nicole <nicole.sattler@live.com>
2018-11-08 11:26:23 -0600

    Add error handling

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const wait = require('gulp-wait')
 
 gulp.task('sass', () => {
   gulp
     .src('fylo-landing-page-496436-master/sass/main.scss')
-    .pipe(sass())
+    .pipe(wait(200))
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('fylo-landing-page-496436-master/css'))
 })
 
 gulp.task('watch:styles', () => {
-  gulp.watch('fylo-landing-page-496436-master/sass/main.scss', ['sass'])
+  gulp.watch('fylo-landing-page-496436-master/sass/**/*.scss', ['sass'])
 })
