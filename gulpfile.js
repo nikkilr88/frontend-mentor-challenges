41752b4718b00c8f035d30a8054ec3a6933a5b62
Nicole <nicole.sattler@live.com>
2018-11-12 11:49:50 -0600

    Update for new project

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ const wait = require('gulp-wait')
 
 gulp.task('sass', () => {
   gulp
-    .src('fylo-landing-page-496436-master/sass/main.scss')
+    .src('fylo-landing-page-227273-master/sass/main.scss')
     .pipe(wait(200))
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('fylo-landing-page-496436-master/css'))
+    .pipe(gulp.dest('fylo-landing-page-227273-master/css'))
 })
 
 gulp.task('watch:styles', () => {
-  gulp.watch('fylo-landing-page-496436-master/sass/**/*.scss', ['sass'])
+  gulp.watch('fylo-landing-page-227273-master/sass/**/*.scss', ['sass'])
 })
